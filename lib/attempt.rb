128864e1d2d227101d9dbce6a4833cf9ebca0001
Alex Flores <domain@me.com>
2018-02-14 13:03:44 -0500

    fixes issue with multi-threaded servers
    see issue #1

--- lib/attempt.rb
+++ lib/attempt.rb
@@ -1,16 +1,14 @@
 module Attempt
 
-  DB = ActiveRecord::Base.connection
-
   def self.is_correct?(query_attempt, challenge_query)
     results_for(query_attempt) == results_for(challenge_query)
   end
 
   def self.results_for(query)
     results = ""
-    DB.transaction do
+    ActiveRecord::Base.connection.transaction do
       begin
-        dataset = DB.execute(query)
+        dataset = ActiveRecord::Base.connection.execute(query)
         results = { :fields => dataset.fields, :rows => dataset.entries }
       rescue Exception => ex
         message = ex.message.gsub("PG::Error: ERROR:  ", "").split("\n").first
